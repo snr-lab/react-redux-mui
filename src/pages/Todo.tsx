4edbdd7da7f3d160e8df3f9e098f5d1df18c5334
Kundu <rahul.kundu@here.com>
2021-03-06 11:20:36 +0530

    Updating page dir structure

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -4,9 +4,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Avatar, Box, CssBaseline, LinearProgress, List, ListItem, Paper, Typography } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 import { ListAlt } from '@material-ui/icons';
-import TaskForm from '../../components/TaskForm';
-import TaskItem from '../../components/TaskItem';
-import { getTodos } from '../../redux/todosSlice';
+import TaskForm from '../components/TaskForm';
+import TaskItem from '../components/TaskItem';
+import { getTodos } from '../redux/todosSlice';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
